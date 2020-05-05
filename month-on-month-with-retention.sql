c1b2f2f6897be9a34027e9c1fde2ef3cc0f565a6
Liron Levy <lirongavrielevy@gmail.com>
2020-05-05 20:31:29 +0300

    added a new transaction metric

---
+++ month-on-month-with-retention.sql
@@ -0,0 +1,446 @@
+
+Declare @ThisWeek TABLE(
+		_year INT,
+		_month INT,
+		ranking INT,
+		cost  FLOAT,
+		impressions FLOAT,
+		clicks FLOAT,
+		signups FLOAT,
+		full_signups FLOAT,
+		Same_Month_FTD FLOAT,
+		Previous_Months_FTD FLOAT,
+		Retention_Purchasers FLOAT,
+		first_deposit_same_month FLOAT,
+		first_deposit_previous_month FLOAT,
+		new_deposit_same_month FLOAT,
+		new_deposit_previous_month FLOAT
+)
+
+
+
+;WITH performance_by_date AS(
+SELECT
+	CONVERT(varchar,performance_by_date.Day,101) AS 'date',
+	YEAR(performance_by_date.[Day]) AS 'year',
+	MONTH(performance_by_date.[Day]) AS 'month',
+	day_number,
+	performance_by_date.cost,
+	performance_by_date.impressions,
+	performance_by_date.clicks,
+	signups_by_date.signups,
+	signups_by_date.full_signups,
+	FTDs_Same_Month.FTD AS 'Same_Month_FTD',
+	FTDs_Previous_Month.FTD AS 'Previous_Months_FTD',
+	Retention_Purchasers.retention_purchasers AS 'Retention_Purchasers',
+	first_purchase_same_month.FirstDeposit AS first_deposit_same_month,
+	first_deposit_previous_month.FirstDeposit AS first_deposit_previous_month,
+	new_deposit_same_month.Deposit AS new_deposit_same_month,
+	new_deposit_previous_month.Deposit AS new_deposit_previous_month
+
+FROM
+
+
+(SELECT
+	[Day],
+	day([Day]) AS day_number,
+	SUM([cost]) AS cost,
+	SUM([Impressions]) AS impressions,
+	SUM([Clicks]) as clicks
+
+FROM dbo.[jubiter-adgroup-report]
+
+WHERE DATEDIFF(month,[Day],GETDATE())<=4 
+GROUP BY [Day]) AS performance_by_date
+
+LEFT JOIN (SELECT
+			transaction_date,
+			SUM(transaction_ranked) AS FTD
+		FROM
+
+			(SELECT
+				transaction_date,
+				month,
+				year,
+				userid,
+				amount,
+				transaction_ranked,
+				COUNT(*) AS duplicate
+
+				FROM
+
+					(SELECT 
+								CONVERT(date,transaction_dt) AS transaction_date,
+								MONTH(transaction_dt) AS 'month',
+								YEAR(transaction_dt) AS 'year',
+								userid,
+								SUM(amount) AS amount,
+								RANK() OVER (PARTITION BY userid ORDER BY transaction_dt ASC) AS transaction_ranked
+
+					FROM dbo.[jubiter-transaction-report] 
+					WHERE transaction_type = 'Buy'
+					GROUP BY transaction_dt,userid) AS i
+
+				GROUP BY
+					transaction_date,
+				userid,
+				amount,
+				transaction_ranked,
+				month,
+				year) AS B
+
+				LEFT JOIN (
+						SELECT
+							reg_dt,
+							MONTH(reg_dt) AS 'month',
+							YEAR(reg_dt) AS 'year',
+							userid 
+							FROM 
+							dbo.[jubiter-reg-report]) AS month_of_reg
+
+			ON b.userid = month_of_reg.userid
+
+
+		WHERE transaction_ranked= 1
+		AND b.month != month_of_reg.month
+		AND b.year >= month_of_reg.year
+	
+		GROUP BY transaction_date) AS FTDs_Previous_Month 
+
+	ON performance_by_date.Day = FTDs_Previous_Month.transaction_date
+------
+	LEFT JOIN (SELECT
+		transaction_date,
+        COUNT(B.userid) AS retention_purchasers
+
+		FROM
+
+			(SELECT
+				transaction_date,
+				month,
+				year,
+				userid,
+				amount,
+				transaction_ranked,
+                transaction_ranked_for_each_month,
+				COUNT(*) AS duplicate
+               --DATEDIFF(month,LAG(transaction_date,1) OVER (PARTITION BY userid ORDER BY transaction_date ASC),transaction_date) AS month_diff_perv_transaction
+
+				FROM
+
+					(SELECT 
+								CONVERT(date,transaction_dt) AS transaction_date,
+								MONTH(transaction_dt) AS 'month',
+								YEAR(transaction_dt) AS 'year',
+								userid,
+								SUM(amount) AS amount,
+								RANK() OVER (PARTITION BY userid ORDER BY transaction_dt ASC) AS transaction_ranked,
+                                RANK() OVER (PARTITION BY userid,YEAR(transaction_dt),MONTH(transaction_dt) ORDER BY transaction_dt ASC) AS transaction_ranked_for_each_month
+
+
+					FROM dbo.[jubiter-transaction-report] 
+                    WHERE transaction_type = 'Buy'
+
+					GROUP BY transaction_dt,userid) AS i
+				GROUP BY
+					transaction_date,
+				userid,
+				amount,
+				transaction_ranked,
+                transaction_ranked_for_each_month,
+				month,
+				year) AS B
+
+				LEFT JOIN (
+						SELECT
+							reg_dt,
+							MONTH(reg_dt) AS 'month',
+							YEAR(reg_dt) AS 'year',
+							userid 
+							FROM 
+							dbo.[jubiter-reg-report]) AS month_of_reg
+
+			ON b.userid = month_of_reg.userid
+
+            WHERE transaction_ranked !=1
+            AND transaction_ranked_for_each_month >= 1
+           --AND month_diff_perv_transaction =0
+
+            GROUP BY transaction_date) AS Retention_Purchasers
+
+	ON performance_by_date.Day = Retention_Purchasers.transaction_date
+
+-----
+LEFT JOIN (SELECT
+			transaction_date,
+			SUM(transaction_ranked) AS FTD
+		FROM
+
+			(SELECT
+				transaction_date,
+				month,
+				year,
+				userid,
+				amount,
+				transaction_ranked,
+				COUNT(*) AS duplicate
+
+				FROM
+
+					(SELECT 
+								CONVERT(date,transaction_dt) AS transaction_date,
+								MONTH(transaction_dt) AS 'month',
+								YEAR(transaction_dt) AS 'year',
+								userid,
+								SUM(amount) AS amount,
+								RANK() OVER (PARTITION BY userid ORDER BY transaction_dt ASC) AS transaction_ranked
+
+					FROM dbo.[jubiter-transaction-report] 
+				
+					GROUP BY transaction_dt,userid) AS i
+
+				GROUP BY
+					transaction_date,
+				userid,
+				amount,
+				transaction_ranked,
+				month,
+				year) AS B
+
+				LEFT JOIN (
+						SELECT
+							reg_dt,
+							MONTH(reg_dt) AS 'month',
+							YEAR(reg_dt) AS 'year',
+							userid 
+							FROM 
+							dbo.[jubiter-reg-report]) AS month_of_reg
+
+			ON b.userid = month_of_reg.userid
+
+
+		WHERE transaction_ranked= 1
+		AND b.month = month_of_reg.month
+		AND b.year = month_of_reg.year
+	
+		GROUP BY transaction_date) AS FTDs_Same_Month 
+
+	ON performance_by_date.Day = FTDs_Same_Month.transaction_date
+
+LEFT JOIN ( 
+	SELECT 
+	reg_dt,
+	SUM(isreg) AS signups,
+	SUM(full_reg) AS full_signups
+
+	FROM
+
+		(SELECT
+
+			CONVERT(date,reg_dt) AS reg_dt,
+			userid,
+			COUNT(*) AS isreg
+
+		FROM dbo.[jubiter-reg-report]
+		GROUP BY reg_dt,userid) AS date_converted
+
+	LEFT JOIN 
+
+		(SELECT 
+
+			userid,
+			fullname,
+			COUNT(*) AS full_reg 
+
+		FROM dbo.[jubiter-reg-report]
+
+		WHERE fullname != ''
+
+		GROUP BY userid,fullname) AS fullname_table
+
+	ON date_converted.userid = fullname_table.userid
+	GROUP BY reg_dt
+
+			 ) AS signups_by_date
+
+ON  performance_by_date.[day] = signups_by_date.reg_dt
+
+/*1st deposit  transaction month = reg month */
+
+
+LEFT JOIN (
+			SELECT
+				 transaction_date,
+				 SUM(amount) AS FirstDeposit
+
+				FROM
+
+					(SELECT
+							CONVERT(date,transaction_dt) AS transaction_date,
+							MONTH(transaction_dt) AS transaction_month,
+							userid,
+							amount,
+							RANK() OVER (PARTITION BY userid ORDER BY transaction_dt ASC) AS transaction_ranked
+
+						FROM dbo.[jubiter-transaction-report] WHERE transaction_type = 'Buy')  AS all_transactions_ranked
+
+				LEFT JOIN
+
+				(SELECT
+					CONVERT(date,reg_dt) AS reg_dt,
+					MONTH(reg_dt) AS reg_month,
+					userid
+				FROM dbo.[jubiter-reg-report] ) AS reg_report
+
+			ON 	all_transactions_ranked.userid = reg_report.userid
+			WHERE transaction_ranked = 1
+			AND transaction_month = reg_month
+			GROUP BY transaction_date) AS first_purchase_same_month
+
+			ON  performance_by_date.[day] = first_purchase_same_month.transaction_date
+
+/*1st deposit  transaction month != reg month */
+
+LEFT JOIN ( SELECT
+			 transaction_date,
+			 SUM(amount) AS FirstDeposit
+
+			FROM
+
+				(SELECT
+					CONVERT(date,transaction_dt) AS transaction_date,
+					MONTH(transaction_dt) AS transaction_month,
+					userid,
+					amount,
+					RANK() OVER (PARTITION BY userid ORDER BY transaction_dt ASC) AS transaction_ranked
+
+				FROM dbo.[jubiter-transaction-report] WHERE transaction_type = 'Buy')  AS all_transactions_ranked
+
+				LEFT JOIN
+
+				(SELECT
+					CONVERT(date,reg_dt) AS reg_dt,
+					MONTH(reg_dt) AS reg_month,
+					userid
+				FROM dbo.[jubiter-reg-report] ) AS reg_report
+
+			ON 	all_transactions_ranked.userid = reg_report.userid
+			WHERE transaction_ranked = 1
+			AND transaction_month != reg_month
+			GROUP BY transaction_date) AS first_deposit_previous_month
+
+ON performance_by_date.Day = first_deposit_previous_month.transaction_date
+
+/*2nd deposit and on transaction month = reg month */
+
+LEFT JOIN (SELECT
+ transaction_date,
+ SUM(amount) AS Deposit
+
+FROM
+
+	(SELECT
+			CONVERT(date,transaction_dt) AS transaction_date,
+			MONTH(transaction_dt) AS transaction_month,
+			userid,
+			amount,
+			RANK() OVER (PARTITION BY userid ORDER BY transaction_dt ASC) AS transaction_ranked
+
+		FROM dbo.[jubiter-transaction-report] WHERE transaction_type = 'Buy')  AS all_transactions_ranked
+
+	LEFT JOIN
+
+		(SELECT
+			CONVERT(date,reg_dt) AS reg_dt,
+			MONTH(reg_dt) AS reg_month,
+			userid
+		FROM dbo.[jubiter-reg-report] ) AS reg_report
+
+	ON 	all_transactions_ranked.userid = reg_report.userid
+	WHERE transaction_ranked != 1
+	AND transaction_month = reg_month
+	GROUP BY transaction_date) AS new_deposit_same_month
+
+ON performance_by_date.Day = new_deposit_same_month.transaction_date
+
+/*2nd deposit and on transaction month != reg month */
+
+LEFT JOIN (SELECT
+ transaction_date,
+ SUM(amount) AS Deposit
+
+FROM
+
+	(SELECT
+			CONVERT(date,transaction_dt) AS transaction_date,
+			MONTH(transaction_dt) AS transaction_month,
+			userid,
+			amount,
+			RANK() OVER (PARTITION BY userid ORDER BY transaction_dt ASC) AS transaction_ranked
+
+		FROM dbo.[jubiter-transaction-report] WHERE transaction_type = 'Buy')  AS all_transactions_ranked
+
+	LEFT JOIN
+
+		(SELECT
+			CONVERT(date,reg_dt) AS reg_dt,
+			MONTH(reg_dt) AS reg_month,
+			userid
+		FROM dbo.[jubiter-reg-report] ) AS reg_report
+
+	ON 	all_transactions_ranked.userid = reg_report.userid
+	WHERE transaction_ranked != 1
+	AND transaction_month != reg_month
+	GROUP BY transaction_date) AS new_deposit_previous_month
+
+ON performance_by_date.Day = new_deposit_previous_month.transaction_date)
+
+INSERT INTO @ThisWeek (_year,_month,ranking,cost,impressions,clicks,signups,full_signups,Same_Month_FTD,Previous_Months_FTD,Retention_Purchasers,first_deposit_same_month,
+		first_deposit_previous_month,
+		new_deposit_same_month,
+		new_deposit_previous_month)
+		
+		SELECT
+				year,
+				month,
+				RANK() OVER (PARTITION BY month ORDER BY year DESC) AS ranking,
+				SUM(cost),
+				SUM(impressions),
+				SUM(clicks),
+				SUM(signups),
+				SUM(full_signups),
+				SUM(Same_Month_FTD),
+				SUM(Previous_Months_FTD),
+				SUM(Retention_Purchasers),
+				SUM(first_deposit_same_month),
+				SUM(first_deposit_previous_month),
+				SUM(new_deposit_same_month),
+				SUM(new_deposit_previous_month)
+			FROM performance_by_date
+			
+			GROUP BY year,month 
+			
+
+SELECT 
+	_year,
+	_month,
+	cost,
+	clicks,
+	ROUND(cost/clicks,2) AS avg_cpc,
+	signups,
+	ROUND(cost/signups,2) AS avg_reg_cost,
+	full_signups,
+	ROUND(cost/full_signups,2) AS avg_full_signups_cost,
+	Same_Month_FTD,
+	Previous_Months_FTD,
+	Retention_Purchasers,
+	(Same_Month_FTD+Previous_Months_FTD) AS 'Total FTD',
+	ROUND(cost/(Same_Month_FTD+Previous_Months_FTD),2) AS avg_firstimepurchaser_cost,
+	first_deposit_same_month,
+	first_deposit_previous_month,
+	new_deposit_same_month,
+	new_deposit_previous_month
+
+
+
+ from @ThisWeek WHERE ranking = 1 ORDER BY _year,_month ASC
